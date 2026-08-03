b573c15d26f68c91d06bcf2b15f3f1cf122b43ea
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2026-08-03 22:10:00 +0000

    fix: stop transformed ancestor shifting fixed model picker

--- src/web/dist/sw.js
+++ src/web/dist/sw.js
@@ -34,7 +34,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'f0ee7c10';
+const CACHE_VERSION = '2c604cda';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
