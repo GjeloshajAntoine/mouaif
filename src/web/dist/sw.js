5404ed476a062d0a055ac6dd1fd4cca41761f389
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2026-08-03 08:42:04 +0000

    fix: extend iOS tab bar to screen edge

--- src/web/dist/sw.js
+++ src/web/dist/sw.js
@@ -34,7 +34,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '5c818edd';
+const CACHE_VERSION = '79f67d09';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
