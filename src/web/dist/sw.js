87d6de94627b44f287d1175f8dc838ff92c43eb7
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2026-08-03 23:10:00 +0000

    fix: keep file modals below mobile status bar

--- src/web/dist/sw.js
+++ src/web/dist/sw.js
@@ -34,7 +34,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'e36ef9b3';
+const CACHE_VERSION = 'e253df32';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
