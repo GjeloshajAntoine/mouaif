a89c49e7df362b2c9f959b29114247c2ccc6952d
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2026-08-03 23:30:00 +0000

    fix: keep file picker close control visible

--- src/web/dist/sw.js
+++ src/web/dist/sw.js
@@ -34,7 +34,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '405b372f';
+const CACHE_VERSION = 'dd748357';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
