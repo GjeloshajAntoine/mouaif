aaad75f63eb28c78a2e5fb6cb902072664827a87
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2026-08-03 22:50:00 +0000

    fix: restore mobile tool modal controls

--- src/web/dist/sw.js
+++ src/web/dist/sw.js
@@ -34,7 +34,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'e36ef9b3';
+const CACHE_VERSION = 'f1c11c20';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
