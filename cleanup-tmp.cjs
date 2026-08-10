662a8ee819de84a72f3ac2700d7cc532ec73b2f9
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2026-08-10 20:23:25 +0000

    chore: remove obsolete debug scripts

--- cleanup-tmp.cjs
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-fs.unlinkSync('C:\\Users\\Admin\\Documents\\mouaif\\build-tmp.cjs');
-console.log('removed');
--- scripts/check-inspector.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const files = fs.readdirSync('frontend/dist/assets').filter((f) => /^index-.*\.js$/.test(f));
-for (const f of files) {
-  const body = fs.readFileSync('frontend/dist/assets/' + f, 'utf8');
-  console.log(f, 'bytes=' + body.length, 'hasCapture=' + body.includes('captureScreenshot'), 'hasPreview=' + body.includes('inspector__preview'));
-}
-const src = fs.readFileSync('frontend/src/components/Inspector.jsx', 'utf8');
-console.log('src bytes=' + src.length, 'hasCapture=' + src.includes('captureScreenshot'));
