5bc9cffa70081a37f6971e627bd570916f8250b8
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2026-08-11 23:40:38 +0000

    build: rebuild frontend bundle with trace/import-in-technical change

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'c95d69b3';
+const CACHE_VERSION = '1081cbee';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
