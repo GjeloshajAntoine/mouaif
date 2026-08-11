d7e72825fa0840169efe807b97ca845ba0fb721e
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2026-08-11 23:46:14 +0000

    build: rebuild frontend bundle with Custom prompts under General

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '54d2f11d';
+const CACHE_VERSION = 'dd853a48';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
