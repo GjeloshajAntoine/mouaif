82a9edf0f523dc1902c1e61a8150e597b9cfe4fa
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2026-08-11 23:44:39 +0000

    build: rebuild frontend bundle without Import chats entry

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'ecf6008d';
+const CACHE_VERSION = '54d2f11d';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
