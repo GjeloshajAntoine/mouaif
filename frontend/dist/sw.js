fe1f3c11829c56f2e35d7c734480bf668bd52bc3
T <t@t>
2026-08-17 20:48:42 +0000

    Revert "feat: compact model picker dropdown rows to single text-height lines"
    This reverts commit 8289708d1c5f44ba612348fd5c46aabaf3f822af.

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '2b2dd2a1';
+const CACHE_VERSION = '9bb868be';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
