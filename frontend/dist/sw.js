752f0d78f3906c114627d0fc0cfb7a7fd41d447c
T <t@t>
2026-08-21 18:08:05 +0000

    fix: anchor PWA root to viewport edges

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '741be89c';
+const CACHE_VERSION = 'c846a672';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
