c7e661185097f18b21bf01386547c69515dc44b3
T <t@t>
2026-08-21 18:32:44 +0000

    fix: cover iOS PWA home indicator area

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'c846a672';
+const CACHE_VERSION = '6092a84e';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
