65f390a1df11f945a57ae95e8af1f627204810b7
T <t@t>
2026-08-24 01:18:38 +0000

    chore: rebuild frontend assets

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '21d84c28';
+const CACHE_VERSION = '321a346b';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
