5ecd36b5d79bb2cb5b66c180bdb8b0de4e8d737d
T <t@t>
2026-08-26 20:32:18 +0000

    fix: hide git count separator

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '16349291';
+const CACHE_VERSION = '29bb0bfb';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
