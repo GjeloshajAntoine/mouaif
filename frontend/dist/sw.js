c2888669ae1eb33a0dae9185b438097453c69080
T <t@t>
2026-08-26 20:48:27 +0000

    fix: contain git counts within toolbar button

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '29bb0bfb';
+const CACHE_VERSION = '1e2600ba';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
