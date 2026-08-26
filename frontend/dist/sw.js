260b4ee4e2768237b53b24e9774e11f220aafef6
T <t@t>
2026-08-26 22:25:21 +0000

    Revert "fix: clarify compact git change badge"
    This reverts commit 59cede36c19f60a11f08c3836c1c56dab0a66f49.

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '89df8cea';
+const CACHE_VERSION = '21d1b1b2';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
