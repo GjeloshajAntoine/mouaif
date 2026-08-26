db562ddcd6ec13263bac8efa3c236bb45b60ba66
T <t@t>
2026-08-26 21:36:54 +0000

    fix: strengthen git count colors

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'c85178aa';
+const CACHE_VERSION = 'cba08abd';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
