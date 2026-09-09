55ee78297f1eed756a3b6e4b062871a3b81019db
T <t@t>
2026-09-09 22:20:00 +0000

    chore: rebuild frontend dist with collapsed hide file content editor

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '76ab494f';
+const CACHE_VERSION = '3eee88bf';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
