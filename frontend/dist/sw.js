c3c4dfe41c999966d6edcddcf2ab707ac3ab9703
T <t@t>
2026-09-09 20:28:00 +0000

    chore: rebuild frontend dist with hide file content page

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '51b9eca3';
+const CACHE_VERSION = '0d61178e';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
