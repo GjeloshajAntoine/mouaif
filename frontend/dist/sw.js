346db43b6783e7617d08a6b8675e2e354d131af5
T <t@t>
2026-09-10 22:12:30 +0000

    chore: rebuild frontend dist with the re-edit fix

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '67a47073';
+const CACHE_VERSION = '45dbac32';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
