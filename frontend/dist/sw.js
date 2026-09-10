6d0a840c524924a2a513c910c3fad665ed648a6c
T <t@t>
2026-09-10 19:18:04 +0000

    chore: rebuild frontend dist with the wide-frame content column

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '4780d170';
+const CACHE_VERSION = '2de45525';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
