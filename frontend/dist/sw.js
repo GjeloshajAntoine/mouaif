336b947f1ac05adc92658b16af2b1dce716c0fc6
T <t@t>
2026-09-10 21:03:36 +0000

    chore: rebuild frontend dist with the shared tool authorization control

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '926bbf23';
+const CACHE_VERSION = '9d0c9f62';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
