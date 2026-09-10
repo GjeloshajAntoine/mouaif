f7d3daac4ee22d2ab5b905db2ffe482c4573c5dd
T <t@t>
2026-09-10 22:44:18 +0000

    chore: rebuild frontend dist with the denser styles panel

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '6a11d9b5';
+const CACHE_VERSION = 'abd0dcdf';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
