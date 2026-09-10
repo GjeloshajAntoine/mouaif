76d5981b46173dc55db8fd33a0bd9bc55aec0159
T <t@t>
2026-09-10 19:24:08 +0000

    chore: rebuild frontend dist with the centred chat rows

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '2de45525';
+const CACHE_VERSION = '092921e4';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
