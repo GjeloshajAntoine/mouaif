56f64770d6f7af8b0ffb8251539cf601b0518439
T <t@t>
2026-09-10 20:05:15 +0000

    chore: rebuild frontend dist with the chat view fixes

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '9601c422';
+const CACHE_VERSION = 'dbcdd449';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
