0dcfaa905a93e982573a14f642d16f69dd48c4d0
T <t@t>
2026-09-11 00:38:18 +0000

    chore: rebuild frontend dist with the inspector selection store

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '0e9c362a';
+const CACHE_VERSION = '14b28d04';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
