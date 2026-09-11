3be755ff53702c24c1d90eb8ac5219b82b0a58d0
T <t@t>
2026-09-11 00:20:44 +0000

    chore: rebuild frontend dist with the inspector edit scope and receipt

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '9b7b3aa4';
+const CACHE_VERSION = '0e9c362a';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
