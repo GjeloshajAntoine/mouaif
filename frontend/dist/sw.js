68c4a0a02f437855c335ebbc114b1f2dbb8a822f
T <t@t>
2026-09-11 18:05:34 +0000

    chore: rebuild frontend dist with the inspector styles fixes

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '9076fdd3';
+const CACHE_VERSION = 'e5899364';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
