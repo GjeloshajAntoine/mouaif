ed49bd366e6e9b9bee568862941c6cede2bc72b7
T <t@t>
2026-09-11 21:46:52 +0000

    chore: rebuild frontend dist with the inspector list changes

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'c914c2a0';
+const CACHE_VERSION = '96df4297';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
