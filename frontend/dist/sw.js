2c3e1cc0ddc9041ab07930fc8062f3d7b714d910
T <t@t>
2026-09-11 20:41:59 +0000

    chore: rebuild frontend dist without the inspector intent chip

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'df04900e';
+const CACHE_VERSION = '15d5276f';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
