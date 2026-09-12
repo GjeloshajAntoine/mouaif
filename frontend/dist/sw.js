af977fd74c8b6f2a0c9cd4dd4ece04597f72e13b
T <t@t>
2026-09-12 22:27:34 +0000

    chore: rebuild frontend dist with the re-proportioned glass orb

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'bc574356';
+const CACHE_VERSION = 'eb3e585b';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
