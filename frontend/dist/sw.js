d10b0902c95283e4c11ad55fc8c4295977366e0c
T <t@t>
2026-09-12 18:49:12 +0000

    chore: rebuild frontend dist with the chat list draft-image fix

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '22be39cb';
+const CACHE_VERSION = 'ce531bd7';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
