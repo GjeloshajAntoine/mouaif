9edf37d395030877425b286376b6e508b0d95ec0
T <t@t>
2026-09-13 12:11:09 +0000

    chore: rebuild frontend dist with the dictation loading states

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '8ef8f73f';
+const CACHE_VERSION = '2f63312a';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
