877b4cba48f5d63e3280c83aedf6037af3a723eb
T <t@t>
2026-09-14 09:11:09 +0000

    chore: rebuild frontend dist with the inspector viewport restore

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '467a96c9';
+const CACHE_VERSION = '89b67787';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
