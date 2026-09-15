0082604f465c70916f4cf0469d82f33ec8c91591
T <t@t>
2026-09-15 23:21:09 +0000

    chore(web): rebuild frontend dist with the one-surface shell card

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'f538474a';
+const CACHE_VERSION = '980c9d3c';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
