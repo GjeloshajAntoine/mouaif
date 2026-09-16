20ef50241e53dde12591329509d1d073d646b3a8
T <t@t>
2026-09-16 20:17:43 +0000

    chore(web): rebuild frontend dist with the named subagent card

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'f4a8a6d4';
+const CACHE_VERSION = 'ef47f02b';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
