b489c60279dfefd207c74a4ddcde5c9ec4326aba
T <t@t>
2026-09-25 18:12:28 +0000

    chore(web): rebuild the served bundle

--- frontend/dist/sw.js
+++ frontend/dist/sw.js
@@ -35,7 +35,7 @@
 
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = '02d8def8';
+const CACHE_VERSION = '9fac71fd';
 const CACHE_NAME = 'mouaif-v' + CACHE_VERSION;
 const SHELL_CACHE = 'mouaif-shell-v' + CACHE_VERSION;
 
